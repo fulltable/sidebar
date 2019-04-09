e0eb46b1e1934ebbabd05e412b045b332b727dfd
Christopher Doornbos <c3doornb@gmail.com>
2019-04-08 17:36:33 -0700

    Update for Proxy.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,4 +4,6 @@ import App from './components/App.jsx';
 import Overview from './components/Overview.jsx';
 
 // ReactDOM.render(<App />, document.getElementById('app'));
-ReactDOM.render(<Overview />, document.getElementById('app'));
+// ReactDOM.render(<Overview />, document.getElementById('app'));
+window.Overview = Overview;
+window.Sidebar = App;
