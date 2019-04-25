83993c2d53319b979f7b4f9cbe4a5f4155bf4aa1
Gary <garyguan0713@gmail.com>
2019-04-25 15:29:45 -0700

    Add '/' route on server side

--- server/aws.js
+++ server/aws.js
@@ -7,6 +7,10 @@ const db = require('../postgresSQL/queries')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', (res, req) => {
+  res.status(200).send('Cool! You connect to AWS EC2')
+})
+
 app.get('/api/sidebar/:id', (req, res) => {
   db.getSidebarById(req.params.id, (data) => {
     res.status(200).json(data[0]);
