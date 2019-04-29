02d1d0687ed50c3566cf706bd15cdbb8b1bbf139
Gary <garyguan0713@gmail.com>
2019-04-29 13:38:53 -0700

    Refactor the sidebar_proxy

--- sidebar_proxy/server/index.js
+++ sidebar_proxy/server/index.js
@@ -7,17 +7,31 @@ const cors = require('cors');
 
 const port = 3003;
 const app = express();
-const db = require('../postgresSQL/queries');
+const db = require('../database/queries');
+// const db_generate = require('..//restaurantsGenerate').populateRestaturant;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/restaurants/:id', express.static(path.join(__dirname, '/public')));
+app.use('/restaurants/:id', express.static(path.join(__dirname, '/../public/')));
 
 app.get('/api/restaurants/:id/info', (req, res) => {
   db.getSidebarById(req.params.id, (data) => {
    res.status(200).json(data[0]);
  });
 });
+
+app.post('/api/restaurants', (req, res) => {
+  // data = db_generate();
+  db.createSidebar(req.body, (result) => {
+    res.status(200).json(result);
+  });
+});
+
+app.delete('/api/restaurants/:id', (req, res)=>{
+  db.deleteRestaurant(req.params.id, (result) => {
+    res.status(200).send(result);
+  });
+})
 
 app.get('/api/restaurants/:id/overview', (req, res) => {
   db.getOverviewById(req.params.id, (data) => {
